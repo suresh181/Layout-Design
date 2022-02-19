e5c1de17063e85da96b57c84dc54ab60a613637c
B Suresh <sursssh@gmail.com>
2022-02-19 21:51:08 +0530

    Commit

---
+++ app/src/main/java/com/example/layout/Dial.kt
@@ -0,0 +1,13 @@
+package com.example.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_dial.*
+
+class Dial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dial)
+
+    }
+}
---
+++ app/src/main/java/com/example/layout/StackAct2.kt
@@ -0,0 +1,11 @@
+package com.example.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StackAct2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stack_act2)
+    }
+}
